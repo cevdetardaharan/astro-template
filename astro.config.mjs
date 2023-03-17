89bf5ea401e8f30952fdc682ae620f46cce52011
Cevdet Arda Haran <cevdet@tutamail.com>
2023-03-17 17:08:31 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://cevdet.me',
-  base: '/astro-template',
-});
+export default defineConfig({});
